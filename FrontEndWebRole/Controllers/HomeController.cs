86faf67674b34dfeaa3e4dbe89ca174986b9012c
Rchua72 <Rhchua@gmail.com>
2015-07-27 18:47:56 +1000

    Initial commit

---
+++ FrontEndWebRole/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FrontEndWebRole.Models;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus;
+
+namespace FrontEndWebRole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Submit");
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: /Home/Submit
+        public ActionResult Submit()
+        {
+            // Get a NamespaceManager which allows you to perform management and
+            // diagnostic operations on your Service Bus queues.
+            var namespaceManager = QueueConnector.CreateNamespaceManager();
+
+            // Get the queue, and obtain the message count.
+            var queue = namespaceManager.GetQueue(QueueConnector.QueueName);
+            ViewBag.MessageCount = queue.MessageCount;
+            return View();
+        }
+
+        // POST: /Home/Submit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Submit(OnlineOrder order)
+        {
+            if (ModelState.IsValid)
+            {
+                // Create a message from the order
+                var message = new BrokeredMessage(order);
+
+                // Submit the order
+                QueueConnector.OrdersQueueClient.Send(message);
+                return RedirectToAction("Submit");
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+    }
+}
